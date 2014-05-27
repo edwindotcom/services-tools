9431a2aa7c87c34d470ada41a5cb98771512247e
edmoz <edwong@mozilla.com>
2014-05-27 16:55:50 -0700

    make it better

--- fxa-oauth-test/multiSmoke.py
+++ fxa-oauth-test/multiSmoke.py
@@ -13,22 +13,26 @@ import os
 import sys
 
 chosen_browsers = [
-    # ('Windows 8.1', 'firefox', '29'),
-    # ('OS X 10.9', 'firefox', '28'),
-    ('Linux', 'firefox', '29'),
+    # ('Windows 7', 'chrome', '34', ''),
+    # ('OS X 10.8', 'chrome', '34', ''),
+    # ('Linux', 'chrome', '34', ''),
+    ('linux', 'android', '4', 'Android Emulator'),
+    # ('OS X 10.8', '', '6', 'iPhone Simulator'),
 ]
 
 user = os.environ.get('PERSONA_SAUCE_USER')
 key = os.environ.get('PERSONA_SAUCE_APIKEY')
 
-OAUTH_RP = "https://123done.dev.lcip.org"
+# OAUTH_RP = "https://123done.dev.lcip.org"
+OAUTH_RP = "http://edwong.mozcloud.org"
 
-btn_signin_locator = "signin"
-input_email = "email"
-input_password = "password"
+btn_signin_locator = "button.signin"
+btn_signup_locator = "button.signup"
+input_email = "input.email"
+input_password = "input.password"
 btn_submit = "submit-btn"
 
-fxa_user = 'fxa.test.user@restmail.net'
+fxa_user = 'fxa.test.acct@restmail.net'
 fxa_password = '12345678'
 
 class FxaOAuthTest(unittest.TestCase):
@@ -43,27 +47,14 @@ class FxaOAuthTest(unittest.TestCase):
                 'browserName': self.br,
                 'version': self.version,
                 'name': self.name,
+                'device': self.deviceName,
+                "selenium-version": "2.41.0"
                 }
         # instantiate the browser
         self.driver = webdriver.Remote(desired_capabilities=des_caps,
                                        command_executor="http://%s:%s@ondemand.saucelabs.com:80/wd/hub"
                                        % (user, key))
-        # this is just handy
         self.driver.implicitly_wait(10)
-
-        # Se1 is just a few changes
-        #des_caps['username'] = user
-        #des_caps['access-key'] = key
-        #des_caps['os'] = des_caps.pop('platform')
-        #des_caps['browser'] = des_caps.pop('browserName')
-        #des_caps['browser-version'] = des_caps.pop('version')
-        #self.browser = selenium(
-        #    'ondemand.saucelabs.com',
-        #    '80',
-        #    json.dumps(des_caps),
-        #    'http://saucelabs.com')
-        #self.browser.start()
-        #self.browser.set_timeout(900000)
 
     def _exc_info(self):
         """Return a version of sys.exc_info() with the traceback frame
@@ -72,28 +63,28 @@ class FxaOAuthTest(unittest.TestCase):
         """
         return sys.exc_info()
 
-    def test_basic_page(self):
-        self.driver.get("http://saucelabs.com/about/team")
-        assert 'Sauce' in self.driver.title
+    # def test_basic_page(self):
+    #     self.driver.get("http://saucelabs.com/about/team")
+    #     assert 'Sauce' in self.driver.title
 
     def test_return_oauth(self):
         self.driver.get(OAUTH_RP)
-        # self.assertTrue('123 Done' in self.driver.title)
-
+        assert '123 Done' in self.driver.title
         self.driver.find_element_by_css_selector(btn_signin_locator).click()
-        self.assertTrue('Firefox' in self.driver.title)
-
+        assert 'Firefox' in self.driver.title
 
         username = self.driver.find_element_by_css_selector(input_email)
         username.send_keys(fxa_user)
         pw = self.driver.find_element_by_css_selector(input_password)
         pw.send_keys(fxa_password)
         self.driver.find_element_by_id(btn_submit).click()
+        # print self.driver.current_url
 
-        btn_is_displayed = self.driver.find_element_by_css_selector(btn_signin_locator).is_displayed()
+        # TODO: Need https in my awsbox instance
+        # btn_is_displayed = self.driver.find_element_by_css_selector(btn_signin_locator).is_displayed()
 
-        self.assertTrue(btn_is_displayed)
-        self.assertTrue('123 Done' in self.driver.title)
+        # self.assertTrue(btn_is_displayed)
+        # self.assertTrue('123 Done' in self.driver.title)
 
     def report_pass_fail(self):
         # Sauce doesn't really know what the test in your end does with the
@@ -114,9 +105,9 @@ class FxaOAuthTest(unittest.TestCase):
 
 # Here's where the magic happens
 classes = {}
-for os, browser, version in chosen_browsers:
+for os, browser, version, deviceName in chosen_browsers:
     # Make a new class name for the actual test cases
-    name = "%s_%s_%s_%s" % (FxaOAuthTest.__name__, os, browser, version)
+    name = "%s_%s_%s_%s_%s" % (FxaOAuthTest.__name__, os, browser, version, deviceName)
     name = name.encode('ascii')
     if name.endswith("."): name = name[:-1]
     for x in ".-_":
@@ -133,6 +124,7 @@ for os, browser, version in chosen_browsers:
               'os': os,
               'br': browser,
               'version': version,
+              'deviceName': deviceName
              })
 
     # append the new class to the classes dict
--- fxa-oauth-test/singleSauceTest.py
+++ fxa-oauth-test/singleSauceTest.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 import unittest
 import os
+import time
 
 user = os.environ.get('PERSONA_SAUCE_USER')
 key = os.environ.get('PERSONA_SAUCE_APIKEY')
 
-OAUTH_RP = "https://123done.dev.lcip.org/"
+OAUTH_RP = "https://123done.dev.lcip.org"
+# OAUTH_RP = "http://edwong.mozcloud.org"
 
 btn_signin_locator = "button.signin"
 btn_signup_locator = "button.signup"
@@ -21,34 +23,41 @@ class ManualTest(unittest.TestCase):
     def setUp(self):
         des_caps = {
                 # The following properties are set dynamically
-                'platform': 'Linux',
-                'browserName': 'firefox',
+                'platform': 'Windows 7',
+                'browserName': 'chrome',
                 'version': '29',
-                'name': 'edwong',
+                'name': 'edwong-test-6',
+                "selenium-version": "2.39.0"
                 }
 
         self.driver = webdriver.Remote(
             desired_capabilities=des_caps,
             command_executor="http://%s:%s@ondemand.saucelabs.com:80/wd/hub"
                 % (user, key))
-        self.driver.implicitly_wait(5)
+        self.driver.implicitly_wait(10)
+        print 'capabilities:'
+        for i in self.driver.capabilities:
+            print i, self.driver.capabilities[i]
 
     def test_basic_page(self):
         self.driver.get(OAUTH_RP)
-        # assert '123 Done' in self.driver.title
+        assert '123 Done' in self.driver.title
         self.driver.find_element_by_css_selector(btn_signin_locator).click()
-        # assert 'Firefox' in self.driver.title
+        assert 'Firefox' in self.driver.title
 
-        # username = self.driver.find_element_by_css_selector(input_email)
-        # username.send_keys(fxa_user)
-        # pw = self.driver.find_element_by_css_selector(input_password)
-        # pw.send_keys(fxa_password)
-        # self.driver.find_element_by_id(btn_submit).click()
+        username = self.driver.find_element_by_css_selector(input_email)
+        username.send_keys(fxa_user)
+        pw = self.driver.find_element_by_css_selector(input_password)
+        pw.send_keys(fxa_password)
+        self.driver.find_element_by_id(btn_submit).click()
+        print self.driver.current_url
+        while('oauth-ui.dev.lcip.org' in self.driver.current_url):
+            print 'wait', self.driver.current_url
+            time.sleep(10)
 
-        # btn_is_displayed = self.driver.find_element_by_css_selector(btn_signin_locator).is_displayed()
-
-        # assert btn_is_displayed
-        # assert '123 Done' in self.driver.title
+        print self.driver.current_url
+        # btn_is_displayed = self.driver.find_element_by_css_selector(btn_submit).is_displayed()
+        assert '123 Done' in self.driver.title
 
     def tearDown(self):
         self.driver.quit()
