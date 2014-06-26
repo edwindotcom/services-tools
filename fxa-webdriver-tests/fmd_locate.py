211c36a019c277d50be6f3a1d03924c63dee43fd
edmoz <edwong@mozilla.com>
2014-06-26 15:45:25 -0700

    add fmd web app script

---
+++ fxa-webdriver-tests/fmd_locate.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import time
+import os
+
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+FXA_ROOT = os.getenv('PUBLIC_URL', "https://accounts.stage.mozaws.net/")
+if FXA_ROOT[-1:] != '/':
+    FXA_ROOT += "/"
+
+FMD_URL = "https://fmd.stage.mozaws.net"
+OAUTH_SIGNIN = "https://accounts.stage.mozaws.net/oauth/"
+
+FXA_SIGNIN = FXA_ROOT + 'signin'
+FXA_SIGNUP = FXA_ROOT + 'signup'
+FXA_SETTINGS = FXA_ROOT + 'settings'
+FXA_DELETE = FXA_ROOT + 'delete_account'
+FXA_CHANGE_PW = FXA_ROOT + 'change_password'
+
+signin_locator = "Sign in"
+btn_signin_locator = "button.signin"
+btn_signup_locator = "button.signup"
+input_email = "input.email"
+input_password = "input.password"
+btn_submit = "submit-btn"
+signed_in_locator = "p.signed-in-email-message"
+signed_in_text = 'You are signed in as'
+
+header_locator = 'h1'
+
+fxa_exist_user = 'fxa.test.acct@restmail.net'
+user_name = fxa_exist_user.split('@')[0]
+fxa_password = '12345678'
+
+
+class FMDTest(unittest.TestCase):
+
+    def setUp(self):
+        des_caps = {'browserName':'firefox'}
+        des_caps['loggingPrefs'] = { 'browser':'ALL' }
+        self.driver = webdriver.Remote(desired_capabilities=des_caps)
+        self.driver.implicitly_wait(60)
+
+    def wait_leave_page(self, current_url):
+        while(self.driver.current_url == current_url):
+            # print 'wait', self.driver.current_url
+            time.sleep(3)
+        return True
+
+    def test_fmd(self):
+        # sign into FMD
+        # click sign in link
+        self.driver.get(FMD_URL)
+        self.driver.find_element_by_link_text(signin_locator).click()
+        self.wait_leave_page(FMD_URL)
+
+        # enter user:pw
+        # assert 'Find' in 
+        time.sleep(5)
+        print self.driver.title
+        username = self.driver.find_element_by_css_selector(input_email)
+        username.send_keys(fxa_exist_user)
+
+        pw = self.driver.find_element_by_css_selector(input_password)
+        pw.send_keys(fxa_password)
+        self.driver.find_element_by_id(btn_submit).click()
+        time.sleep(5)
+
+        # should be on finding phone page
+        header = self.driver.find_element_by_css_selector(header_locator)
+        print 'header', header.get_attribute("innerHTML")
+        # assert(user_name in header.get_attribute("innerHTML"))
+
+        time.sleep(30)
+        print 'console: You need a fairly recent version of webdriver'
+        for entry in self.driver.get_log('browser'):
+            print entry
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
+# parser = OptionParser(usage="usage: %prog [options] runType {smoke|oauth|full}")
+# parser.add_option("-p", "--platforms",
+#                   help="comma seperated list of platforms to run: %s" % platform_types)
+# parser.add_option("--local",
+#                   action='store_true',
+#                   help="option to run against run_local selenium server")
+# options, args = parser.parse_args()
+
+# if not args:
+#     print 'run type not defined: %s' % run_types
+#     sys.exit(1)
+
+# if options.local:
+#     run_local = True
+
+# run_type = 'smoke'
+
